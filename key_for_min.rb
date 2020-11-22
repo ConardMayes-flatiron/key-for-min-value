54f303cf2a9f7971c6fe5ea6a5d5a82ada798290
ConardMayes-flatiron <ConradAsatoMayes+flatironschool@gmail.com>
2020-11-22 13:01:12 -0600

    commit tree cfaef13b9d7dc85b294d39b66b3d3bdc94c0aa0a

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,19 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+require "pry"
 
 def key_for_min_value(name_hash)
-
+    lowest_value_name = ""
+    name_hash.collect do |name,age|
+        if lowest_value_name == ""
+            lowest_value_name = "#{name}"
+        elsif age < name_hash[:"#{lowest_value_name}"]
+            lowest_value_name = name
+        end
+    end
+    if lowest_value_name == ""
+        nil
+    else
+        return lowest_value_name.to_sym
+    end
 end
